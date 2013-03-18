df1716abbd29eb6775791ba0de25767b317e7097
Kenneth Wong <wong.jj.kenneth@gmail.com>
2013-03-19 04:38:22 +0800

    Major Changes to UI
    Ui updates.
    made ui dynamic
    colour scheme changes
    jsonp calls confirmed to be working
    ace editor stored in local src

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -2,16 +2,20 @@ $(document).ready(function() {
     // Run files
     var editor = ace.edit("editor");
     setupEditor();
+
+    call("http://cloud-mreduce.ap01.aws.af.cm/","callback=?");
     //-----------
 
     function setupEditor() {
-
-        editor.setTheme("ace/theme/eclipse");
+        editor.setValue("function(){ console.log('hello world!')}");
+        editor.setTheme("ace/theme/idle_fingers");
         editor.getSession().setMode("ace/mode/javascript");
         editor.setShowPrintMargin(false);
         editor.setHighlightActiveLine(true);
         editor.resize();
+        editor.setBehavioursEnabled(true);
         editor.getSession().setUseWrapMode(true);
+        document.getElementById('editor').style.fontSize='16px';
     }
 
     $("#run").click(function() {
@@ -21,7 +25,29 @@ $(document).ready(function() {
         editor.setValue("");
         console.log('reset');
     });
-    
-    
+
+    function call(url, param){;
+        $.getJSON(url, param, function(data) {
+            console.log(JSON.stringify(data));
+        });
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });

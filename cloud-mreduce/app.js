1f4d03d5672fd5789f154288494d81037480ba30
kenwjj <wong.jj.kenneth@gmai.com>
2013-03-26 18:06:05 +0800

    socket.io presence working in lobby

--- cloud-mreduce/app.js
+++ cloud-mreduce/app.js
@@ -1,7 +1,7 @@
-
 /**
  * Module dependencies.
  */
+
 
 var express = require('express')
   , routes = require('./routes')
@@ -14,54 +14,94 @@ var express = require('express')
   , querystring = require('querystring')
   , app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.enable("jsonp callback");
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+var express = require('express'),
+    routes = require('./routes'),
+    user = require('./routes/user'),
+    http = require('http'),
+    https = require('https'),
+    path = require('path'),
+    url = require('url'),
+    btoa = require('btoa'),
+    querystring = require('querystring'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io').listen(server);
+
+// Switch socket on, emit news data
+io.sockets.on('connection', function(socket) {
+    console.log("Client Connected");
+
+    socket.on('connect', function(username) {
+        socket.set('username', username);
+        console.log(username);
+        io.sockets.emit('connect', username);
+    });
+
+    socket.on('disconnect', function(username) {
+        socket.get('username', function(err, username) {
+            io.sockets.emit("removeUser", username);
+        });
+    });
+
+    // socket.emit('news', {
+    //     hello: 'world'
+    // });
+
+    socket.on('my other event', function(data) {
+        console.log(data);
+    });
+
+});
+app.configure(function() {
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.enable("jsonp callback");
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
+app.configure('development', function() {
+    app.use(express.errorHandler());
 });
 
 // Require Mongoose module to handle mongo connection with DB
 // Require schema for question model
-var mongoose = require('mongoose'), question = require('./models/question'), user = require('./models/user');
+var mongoose = require('mongoose'),
+    question = require('./models/question'),
+    user = require('./models/user');
 
 // Establish connection with mongolab DB
 mongoose.connect('mongodb://cloud-mreduce:cloudmr123@ds053317.mongolab.com:53317/cloud-mreduce');
 
 // Redirect to /public/web from root domain
 app.get('/', function(req, res) {
-	res.redirect('/web');
+    res.redirect('/web');
 });
 
 // API to get the question model object from the DB based on specified question ID
 app.get('/questions', function(req, res) {
-	var _get = url.parse(req.url, true).query;
-	var question_id = _get['id'];
-	question.findOne({
-		question_id : question_id
-	}, function(err, selected_question) {
-		if (err) {
-			res.jsonp("error");
-		}
-		
-		// Requested question found
-		if (selected_question !== null) {
-			// Returns question
-			res.jsonp(selected_question);
-		} else {
-			res.jsonp("not found");
-		}
-	});
+    var _get = url.parse(req.url, true).query;
+    var question_id = _get['id'];
+    question.findOne({
+        question_id: question_id
+    }, function(err, selected_question) {
+        if (err) {
+            res.jsonp("error");
+        }
+
+        // Requested question found
+        if (selected_question !== null) {
+            // Returns question
+            res.jsonp(selected_question);
+        } else {
+            res.jsonp("not found");
+        }
+    });
 });
 
 // API to hanlde Facebook login
@@ -121,121 +161,145 @@ app.get('/fb_login', function(req, res) {
 	}).on('error', function(e) {
 		console.log("Got error: " + e.message);
 	});
+
+    var _get = url.parse(req.url, true).query;
+    var access_token = _get['access_token'];
+    var options = {
+        host: 'graph.facebook.com',
+        path: '/me?fields=name,username,email,birthday&access_token=' + access_token,
+        method: 'GET'
+    };
+    res.set('Content-Type', 'application/json');
+    var fb_data = '';
+    https.get(options, function(response) {
+        console.log("Got response: " + response.statusCode);
+
+        response.on("data", function(chunk) {
+            fb_data += chunk.toString();
+        });
+
+        // Process response from graph api
+        response.on("end", function() {
+            var json = JSON.parse(fb_data);
+            console.log(json);
+            user.findOne({
+                username: json.username
+            }, function(err, current_user) {
+                if (err) {
+                    res.jsonp('error');
+                }
+                if (current_user !== null) {
+                    current_user.returning = true;
+                    res.jsonp(['returning user', current_user]);
+                } else {
+                    var new_user = new user();
+                    new_user.id = json.id;
+                    new_user.name = json.name;
+                    new_user.username = json.username;
+                    new_user.email = json.email;
+                    new_user.birthday = json.birthday;
+                    new_user.save();
+                    new_user.returning = false;
+                    res.jsonp(['new user', new_user]);
+                }
+            });
+        });
+    }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+    });
 });
 
 // Code verification API using GET method
 app.get('/verify', function(req, res) {
-	// parse the URL query
-	var _get = url.parse(req.url, true).query;
-	
-	// Question ID param
-	var question_id = _get['q_id'];
-	
-	// Language param, either 'py' or 'js'
-	var lang = _get['lang'];
-	if (lang === 'py') {
-		lang = 'python';
-	}
-	
-	// Solution param, urlEncoded
-	var solution = _get['solution'];
-	console.log(solution);
-	
-	// Find the question model object based on question ID
-	question.findOne({
-		question_id : question_id
-	}, function(err, selected_question) {
-		if (err) {
-			res.jsonp("error");
-		}
-		
-		// Requested question found
-		if (selected_question !== null) {
-			// Prepare jsonrequest data
-			var data = {
-				solution:solution,
-				tests: (lang === 'js') ? selected_question.js_tests : selected_question.py_tests
-			};
-			json_data = querystring.stringify({jsonrequest:JSON.stringify(data)});
-			console.log(json_data);
-			
-			var verified_results = '';
-			// HTTP request options
-			var options = {
-				host : 'ec2-54-251-193-188.ap-southeast-1.compute.amazonaws.com',
-				//path : '/' + lang + '?jsonrequest=' + btoa(JSON.stringify(data)),
-				path : '/' + lang,
-				method : 'POST',
-				headers: {
-        			'Content-Type': 'application/x-www-form-urlencoded',
-        			'Content-Length': json_data.length
-    			}
-			};
-			
-			// Call the HTTP request
-			var request = http.request(options, function(response) {
-    			// Handle data received
-    			response.on('data', function (chunk) {
-        			verified_results += chunk.toString();
-    			});
-    			// Send the json response
-    			response.on("end", function() {
-					res.jsonp(JSON.parse(verified_results));
-					console.log(options);
-				});
-			}).on('error', function(e) {
-				console.log("Got error: " + e.message);
-			});
-			
-			// Write jsonrequest data to the HTTP request
-			request.write(querystring.stringify({jsonrequest:JSON.stringify(data)}));
-			request.end();
-		} else {
-			res.jsonp("not found");
-		}
-	});
+    // parse the URL query
+    var _get = url.parse(req.url, true).query;
+
+    // Question ID param
+    var question_id = _get['q_id'];
+
+    // Language param, either 'py' or 'js'
+    var lang = _get['lang'];
+    if (lang === 'py') {
+        lang = 'python';
+    }
+
+    // Solution param, urlEncoded
+    var solution = _get['solution'];
+    console.log(solution);
+
+    // Find the question model object based on question ID
+    question.findOne({
+        question_id: question_id
+    }, function(err, selected_question) {
+        if (err) {
+            res.jsonp("error");
+        }
+
+        // Requested question found
+        if (selected_question !== null) {
+            // Prepare jsonrequest data
+            var data = {
+                solution: solution,
+                tests: (lang === 'js') ? selected_question.js_tests : selected_question.py_tests
+            };
+            json_data = querystring.stringify({
+                jsonrequest: JSON.stringify(data)
+            });
+            console.log(json_data);
+
+            var verified_results = '';
+            // HTTP request options
+            var options = {
+                host: 'ec2-54-251-193-188.ap-southeast-1.compute.amazonaws.com',
+                //path : '/' + lang + '?jsonrequest=' + btoa(JSON.stringify(data)),
+                path: '/' + lang,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': json_data.length
+                }
+            };
+
+            // Call the HTTP request
+            var request = http.request(options, function(response) {
+                // Handle data received
+                response.on('data', function(chunk) {
+                    verified_results += chunk.toString();
+                });
+                // Send the json response
+                response.on("end", function() {
+                    res.jsonp(JSON.parse(verified_results));
+                    console.log(options);
+                });
+            }).on('error', function(e) {
+                console.log("Got error: " + e.message);
+            });
+
+            // Write jsonrequest data to the HTTP request
+            request.write(querystring.stringify({
+                jsonrequest: JSON.stringify(data)
+            }));
+            request.end();
+        } else {
+            res.jsonp("not found");
+        }
+    });
 });
 
 // API to retrieve total number of questions in the DB
 app.get('/total_questions', function(req, res) {
-	question.find(
-        {},
-        function(err, docs) {
-        	if (!err){ 
-            	res.jsonp({number:docs.length});
-            } else {
-				throw err;
-			}
-		}
-    );
+    question.find({}, function(err, docs) {
+        if (!err) {
+            res.jsonp({
+                number: docs.length
+            });
+        } else {
+            throw err;
+        }
+    });
 });
 
-var server = http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
 
-var io = require('socket.io').listen(server);
-  
-// Switch socket on, emit news data
-io.sockets.on('connection', function (socket) {
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
-		console.log(data);
-	});
-	user.find({},
-		function(err, docs) {
-			if (!err) {
-				var online_users = [];
-				for (var i = 0; i < docs.length; i++) {
-					var doc = docs[i];
-					if (doc.online) {
-						online_users.push(doc);
-					}
-				}
-				socket.emit('online users',online_users);
-				console.log(online_users);
-			} else {
-				console.log('error');
-			}
-		});
+server.listen(app.get('port'), function() {
+    console.log("Express server listening on port " + app.get('port'));
 });
--- cloud-mreduce/public/assets/js/lobby.js
+++ cloud-mreduce/public/assets/js/lobby.js
@@ -1,21 +1,68 @@
 $(document).ready(function() {
 
     // ----------------------------- Variables -----------------------------
-
-
+    var lobbyendpoint = "http://localhost:3000/";
+    var server = io.connect(lobbyendpoint);
     // ----------------------------- Setup files -----------------------------
-
+    connect();
+    loadUser();
+    removeUser();
 
 
     // ----------------------------- Click Listeners -----------------------------
 
     $("#create").click(function() {
-        alert('hey');
+        var form = $('#createroomForm');
+        var lang = $('.control-group input[name=languageoptions]:checked').val();
+        var pos = $('.control-group input[name=positionoptions]:checked').val();
+        var username = "kenneth"
+
+        //Insert ajax request here
+        //On success create an entry in the table
+        if (pos == "mapper") {
+            $('#lobby tr:last').after("<tr><td>" + username + "</td><td></td><td>" + lang + "</td><td><a href='collaborate.html'><button class='btn btn-success'>Join</button></a></td></tr>");
+
+        } else if (pos == "reducer") {
+            $('#lobby tr:last').after("<tr><td></td><td>" + username + "</td><td>" + lang + "</td><td><a href='collaborate.html'><button class='btn btn-success'>Join</button></a></td></tr>");
+        }
+
     });
 
     // ----------------------------- Methods -----------------------------
 
-   
+
+
+    function loadUser() {
+        server.on('connect', function(data) {
+            if (data != null) {
+                $('#onlineUsers tr:last').after('<tr id='+data+'><td>' + data + '</td></tr>');
+            }
+        });
+    }
+
+    function loadtables() {
+
+        server.on('create', function(lobby) {
+            createRoom();
+        });
+    }
+
+    function createRoom() {
+
+    }
+
+    function connect() {
+        var username = $.now();
+        server.emit('connect', username);
+
+    }
+
+    function removeUser() {
+        server.on('removeUser', function(name) {
+            $('#'+name).remove();
+        });
+    }
+
 
     // ----------------------------- Utility -----------------------------
 

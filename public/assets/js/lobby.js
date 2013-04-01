7131ba3c914c189e9550a325ee936751086c60f5
kenwjj <wong.jj.kenneth@gmai.com>
2013-04-01 13:25:29 +0800

    Enabled Facebook

--- public/assets/js/lobby.js
+++ public/assets/js/lobby.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
 
-	// function get_username(cb) {
-	// 	FB.login(function(response) {
-	// 		if (response.authResponse) {
-	// 			var token = response.authResponse.accessToken;
-	// 			FB.api('/me', function(response) {
-	// 				cb(response.name, response.id);
-	// 			});
-	// 		}
-	// 	});
-	// }
+	function get_username(cb) {
+		FB.login(function(response) {
+			if (response.authResponse) {
+				var token = response.authResponse.accessToken;
+				FB.api('/me', function(response) {
+					cb(response.name, response.id);
+				});
+			}
+		});
+	}
 
-	// get_username(function(user_name, user_id) {
+	get_username(function(user_name, user_id) {
 		// ----------------------------- Variables -----------------------------
 		var lobbyendpoint = "/";
 		var server = io.connect(lobbyendpoint);
-		var username = $.now();
-		// var username = user_name;
-		// var userid = user_id;
-		// console.log(user_name);
-		// console.log(user_id);
+		// var username = $.now();
+		var username = user_name;
+		var userid = user_id;
+		console.log(user_name);
+		console.log(user_id);
 
 		// ----------------------------- Setup files -----------------------------
 		connect();
@@ -332,6 +332,6 @@ $(document).ready(function() {
 
 		}
 
-	// });
+	});
 
 });

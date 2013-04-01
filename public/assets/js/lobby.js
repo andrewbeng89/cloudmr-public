ea6712c2b002d89448d809cfbda815552ca38dd7
andrewbeng89 <madbeng@gmail.com>
2013-04-01 10:04:43 +0800

    testing session.socket

--- public/assets/js/lobby.js
+++ public/assets/js/lobby.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 
 	get_username(function(user_name) {
 		// ----------------------------- Variables -----------------------------
-		var lobbyendpoint = "/";
+		var lobbyendpoint = "/web/";
 		var server = io.connect(lobbyendpoint);
 		var username = user_name;
 		console.log(user_name);

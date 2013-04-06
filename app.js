ccf74ebc28ac648ac53ccc665990e31948368afb
andrewbeng89 <madbeng@gmail.com>
2013-04-06 10:07:24 +0800

    testing breaking change

--- app.js
+++ app.js
@@ -524,5 +524,5 @@ sessionSockets.on('connection', function(err, socket, session) {
 
 server.listen(app.get('port'), function() {
 	console.log("Express server listening on port " + app.get('port'));
-	console.log("node.js debug log");
+	//console.log("node.js debug log");
 });

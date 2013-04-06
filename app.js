7c86323e7dc090b349d2f4107cdabd2e6ab62f37
andrewbeng89 <madbeng@gmail.com>
2013-04-07 02:48:56 +0800

    added tests for app.js

--- app.js
+++ app.js
@@ -526,3 +526,12 @@ server.listen(app.get('port'), function() {
 	console.log("Express server listening on port " + app.get('port'));
 	console.log("node.js debug log");
 });
+
+exports.testExpressApp = function(beforeExit, assert) {
+	assert.response(server, {
+    	url: '/questions',
+    	method: 'GET'
+	}, {
+    	status: 200
+	});
+}

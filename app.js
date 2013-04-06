728648add9039a3b22c387d2dc638e650dda693a
andrewbeng89 <madbeng@gmail.com>
2013-04-07 00:43:32 +0800

    breaking changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express = require('express')
 	, connect = require('connect')
 	, url = require('url')
 	, querystring = require('querystring')
-	, app = express();
+	//, app = express();
 
 // Require Mongoose module to handle mongo connection with DB
 // Require schema for question model
@@ -522,7 +522,7 @@ sessionSockets.on('connection', function(err, socket, session) {
 
 });
 
-//server.listen(app.get('port'), function() {
+server.listen(app.get('port'), function() {
 	console.log("Express server listening on port " + app.get('port'));
 	console.log("node.js debug log");
 });

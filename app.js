ee78880fc554f8e2541e301f3b95afe0180d8f52
andrewbeng89 <madbeng@gmail.com>
2013-04-07 03:17:37 +0800

    added test

--- app.js
+++ app.js
@@ -526,3 +526,7 @@ server.listen(app.get('port'), function() {
 	console.log("Express server listening on port " + app.get('port'));
 	console.log("node.js debug log");
 });
+
+exports.test = function(beforeExit, assert) {
+    assert.equal(6, 'foobar'.length);
+};

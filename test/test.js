31f94952dd06b7c25aba38af0c00ebd1b85f0807
andrewbeng89 <madbeng@gmail.com>
2013-04-07 17:13:34 +0800

    using mocha for tests

--- test/test.js
+++ test/test.js
@@ -1,3 +1,26 @@
-exports.testsStringLength = function(beforeExit, assert) {
-    assert.equal(6, 'foobar'.length);
-};
+/*
+ var app = require('../app')
+ , http = require('http');
+
+ describe('User API', function() {
+
+ before(function(done) {
+ server = http.createServer(app);
+ });
+
+ it('GET /questions should return 200', function(done) {
+ request()
+ .get('/questions')
+ .end(function(res){
+ assert(res.ok);
+ done();
+ });
+ });
+
+ }); */
+
+describe('app', function() {
+	describe('GET /questions', function() {
+		it('respond not found message');
+	});
+}); 

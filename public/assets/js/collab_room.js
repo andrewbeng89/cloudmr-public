875a9cd31701e59a558e463c9689b518a8ac4f16
kenwjj <wong.jj.kenneth@gmai.com>
2013-04-10 15:11:40 +0800

    Alert on both join

--- public/assets/js/collab_room.js
+++ public/assets/js/collab_room.js
@@ -185,6 +185,7 @@ $(document).ready(function() {
             takeSide();
 
             if (roomCount == 2) {
+                alert("yo");
                 room.roomCount = roomCount;
                 room.pos = pos;
                 server.emit('closeRoom', room);
